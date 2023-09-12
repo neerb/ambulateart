6b70362def2d1ac2c9014c4898f1ab740cdecfa6
Nathan <31359612+neerb@users.noreply.github.com>
2023-09-11 20:37:00 -0500

    updates to about section

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Fade from "react-reveal";
 import "./About.css";
 import Draggable from 'react-draggable';
@@ -10,6 +10,77 @@ const skillPics = [
   'ambulateart/images/skillpics/gatsbyskill.png'
 ]
 let skillPos = 0;
+
+function SkillCard(props) {
+  let minimizedStylesHandle = 'handle box-shadow';
+  let maximizedStylesHandle = 'handle';
+  let skillCardNoShadow = 'skills-image-window';
+  let skillCardShadow = 'skills-image-window box-shadow';
+
+  const [minimized, setMinimized] = useState(false);
+
+  function handleMinimize(e) {
+    setMinimized(!minimized);
+  }
+
+  return (
+    // <div className="skill-img-label">
+    <Draggable
+      handle=".handle"
+      defaultPosition={{ x: 0, y: 0 }}
+      scale={1}>
+      <div className={minimized ? skillCardNoShadow : skillCardShadow}>
+        <div className={minimized ? minimizedStylesHandle : maximizedStylesHandle}>
+          <button className='minimize-btn' style={{ backgroundColor: minimized ? "gold" : "red" }} onClick={handleMinimize}></button>
+          <div className='program-title'>
+            <div className='program-line'></div>
+            <h1 className='skills-text-title'>{props.skill.name}</h1>
+            <div className='program-line'></div>
+          </div>
+        </div>
+        <div className='image-skill-wrapper' style={{ visibility: minimized ? 'hidden' : 'visible' }}>
+          <img className='image-skill' alt='skill' src={"ambulateart/images/skillpics/" + props.skill.src}></img>
+        </div>
+      </div>
+    </Draggable>
+    // </div>
+  );
+}
+
+function SkillTextCard(props) {
+  let minimizedStylesHandle = 'text-handle box-shadow';
+  let maximizedStylesHandle = 'text-handle';
+  let skillCardNoShadow = 'skills-text-bubble';
+  let skillCardShadow = 'skills-text-bubble box-shadow';
+  const [minimized, setMinimized] = useState(false);
+
+  function handleMinimize(e) {
+    setMinimized(!minimized);
+  }
+
+  return (
+    <Draggable
+      handle=".text-handle"
+      defaultPosition={{ x: 0, y: 0 }}
+      scale={1}>
+      <div className={minimized ? skillCardNoShadow : skillCardShadow}>
+        <div className={minimized ? minimizedStylesHandle : maximizedStylesHandle}>
+          <button className='minimize-btn' onClick={handleMinimize} style={{ backgroundColor: minimized ? "gold" : "red" }}></button>
+          <div className='program-title'>
+            <div className='program-line'></div>
+            <h1 className='skills-text-title'>{props.programText}</h1>
+            <div className='program-line'></div>
+          </div>
+        </div>
+        <div className='skills-text-wrapper' style={{ visibility: minimized ? 'hidden' : 'visible' }}>
+          <h1 className='skills-text-bubble-text'>{props.titleText}</h1>
+          <p>{props.skillsText}</p>
+
+        </div>
+      </div>
+    </Draggable>
+  );
+}
 
 class About extends Component {
   constructor(props) {
@@ -48,107 +119,39 @@ class About extends Component {
 
     const skills = this.props.skills.skills.map((skills) => {
       return (
-        <div className="skill-img-label">
-          <Draggable
-            handle=".handle"
-            defaultPosition={{ x: 0, y: 0 }}
-            scale={1}>
-            <div className='skills-text-bubble'>
-              <div className='handle'>
-                <button className='minimize-btn' onClick={null}></button>
-                <div className='program-title'>
-                  <div className='program-line'></div>
-                  <h1 className='skills-text-title'>{skills.name}</h1>
-                  <div className='program-line'></div>
-                </div>
-              </div>
-              <div className='image-skill-wrapper'>
-                <img className='image-skill' alt='skill' src={"ambulateart/images/skillpics/" + skills.src}></img>
-              </div>
-            </div>
-          </Draggable>
-        </div>
+        <SkillCard skill={skills}></SkillCard>
       );
     });
 
     return (
       <div id="about" className='about'>
         <Fade>
-          <div className="can-work">
-            <h1 className="can-work-text">I can work <span>with</span></h1>
-          </div>
           <div className='about-wrapper'>
-            <div className='about-outer-skills'>
-              <div className='skill-windows'>
-                <div className="skills-list-windows">{skills}</div>
-                <div className="skills-text">
-                  <Draggable
-                    handle=".handle"
-                    defaultPosition={{ x: 0, y: 0 }}
-                    position={null}
-                    grid={null}
-                    scale={1}>
-                    <div className='skills-text-bubble'>
-                      <div className='handle'>
-                        <button className='minimize-btn' onClick={null}></button>
-                        <div className='program-title'>
-                          <div className='program-line'></div>
-                          <h1 className='skills-text-title'>Skills 1</h1>
-                          <div className='program-line'></div>
-                        </div>
-                      </div>
-                      <h1 className='skills-text-bubble-text'>Programming Languages</h1>
-                      <p>JavaScript, TypeScript, Java, C#, C++</p>
-                    </div>
-                  </Draggable>
-
-                  <hr></hr>
-                  <Draggable
-                    handle=".handle"
-                    defaultPosition={{ x: 0, y: 0 }}
-                    position={null}
-                    grid={null}
-                    scale={1}>
-                    <div className='skills-text-bubble'>
-                      <div className='handle'>
-                        <button className='minimize-btn' onClick={null}></button>
-                        <div className='program-title'>
-                          <div className='program-line'></div>
-                          <h1 className='skills-text-title'>Skills 2</h1>
-                          <div className='program-line'></div>
-                        </div>
-                      </div>
-                      <h1 className='skills-text-bubble-text'>Technologies/Frameworks</h1>
-                      <p>React.js, RESTful APIs (HTTP querying), HTML, CSS, SASS, Bootstrap, JQuery, GatsbyJS, TailwindCSS, MongoDB, SQL</p>
-                    </div>
-                  </Draggable>
-
-                  <hr></hr>
-
-                  <Draggable
-                    handle=".handle"
-                    defaultPosition={{ x: 0, y: 0 }}
-                    position={null}
-                    grid={null}
-                    scale={1}>
-                    <div className='skills-text-bubble'>
-                      <div className='handle'>
-                        <button className='minimize-btn' onClick={null}></button>
-                        <div className='program-title'>
-                          <div className='program-line'></div>
-                          <h1 className='skills-text-title'>Skills 3</h1>
-                          <div className='program-line'></div>
-                        </div>
-                      </div>
-                      <h1 className='skills-text-bubble-text'>Miscellaneous</h1>
-                      <p>GitHub Version Control, Jira Software Project/Ticketing Management, Graphic Design & Photoshop, Ambulating</p>
-                    </div>
-                  </Draggable>
-
-                </div>
+            <Fade top>
+              <div className="can-work">
+                <h1 className="can-work-text">I work <span>with...</span></h1>
               </div>
+            </Fade>
+            {/* <div className='about-outer-skills'> */}
+            <div className='skill-windows'>
+              <Fade left>
+                <div className="skills-list-windows">{skills}</div>
+              </Fade>
+
+              <Fade right>
+                <div className="skills-text">
+                  <SkillTextCard programText="Skills" titleText="Programming Languages" skillsText="JavaScript, TypeScript, Java, C#, C++" />
+                  <hr></hr>
+                  <SkillTextCard programText="Skills" titleText="Technologies/Frameworks" skillsText="React.js, RESTful APIs (HTTP querying), HTML, CSS, SASS, Bootstrap, JQuery, GatsbyJS, TailwindCSS, MongoDB, SQL" />
+                  <hr></hr>
+                  <SkillTextCard programText="Skills" titleText="Miscellaneous" skillsText="GitHub Version Control, Jira Software Project/Ticketing Management, Graphic Design & Photoshop, Ambulating" />
+                  <hr></hr>
+                  <SkillTextCard programText="!!!" titleText="Click & Drag us" skillsText=":}" />
+                </div>
+              </Fade>
             </div>
           </div>
+          {/* </div> */}
         </Fade>
       </div>
     );
