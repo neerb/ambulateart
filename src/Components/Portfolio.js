90ee9e6cc20294ccec15729641dfda4efc917b0f
Nathan <31359612+neerb@users.noreply.github.com>
2023-09-15 10:29:47 -0500

    created portfolio section

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -13,16 +13,75 @@ import Gallery from "react-photo-gallery";
 import "./Portfolio.css";
 
 let id = 0;
+
+function SoftwareProjectCard(props) {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
 class Portfolio extends Component {
+  constructor(props) {
+    super(props);
+
+    // this.state = {
+    //   frontEndProjects: [this.props.data.software[0],
+    //                     this.props.data.software[1]],
+    //   backEndProjects: []
+    // }
+
+  }
+
+  componentDidMount() {
+
+    // console.log(this.state.frontEndProjects);
+  }
+
+
   render() {
     if (!this.props.data) return null;
 
     return (
       <div id="portfolio">
-        <Fade left duration={1000} distance="40px">
-          <h1 className='test'>Software Projects. All websites are fully mobile responsive, try them out.</h1>
+        <Fade left>
+          <p>Front-End Projects</p>
+          <div className="projects-one">
+            
+            <Fade left duration={500}>
+              <div>
+                <div className="p1-1-container">
+                    <SoftwareProjectCard/>
+                </div>
+              </div>
+            </Fade>
 
+            <Fade left duration={750}>
+              <div>
+                <div className="p1-2-container">
+                  <SoftwareProjectCard/>
+                </div>
+              </div>
+            </Fade>
 
+            <Fade left duration={1000}>
+              <div>
+                <div className="p1-3-container">
+                    <SoftwareProjectCard/>
+                </div>
+              </div>
+            </Fade>
+          </div>
+        </Fade>
+
+        <Fade right>
+          <p>Back-End Projects</p>
+          <div className="projects-two">
+            <div className="p2-container">
+
+            </div>
+          </div>
         </Fade>
       </div >
     );
